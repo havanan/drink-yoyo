71a1697e0bb79febca11db3005374493ff24ec2a
An <havanan95@gmail.com>
2019-09-11 17:07:43 +0700

    sửa size

--- resources/views/frontend/home/bill_barcode.blade.php
+++ resources/views/frontend/home/bill_barcode.blade.php
@@ -1,43 +1,22 @@
 <script type="text/javascript" src="{{asset('yoyo')}}/javascript/tether.min.js"></script>
 <script type="text/javascript" src="{{asset('yoyo')}}/javascript/bootstrap.min.js"></script>
 <script src="{{asset('js/payment.js')}}"></script>
-<style>
-    address{
-        margin-bottom: 0;
-    }
-    .invoice-title h2, .invoice-title h3 {
-        display: inline-block;
-    }
-</style>
-<div class="container body-bill" id="billPrint">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="table-responsive">
-                        @if(isset($cartInfo['list']) && count($cartInfo) > 0)
+
+<div class="" id="billPrint">
+     @if(isset($cartInfo['list']) && count($cartInfo) > 0)
                             @foreach($cartInfo['list'] as $key => $item)
                                 @if($item[0]['qty'] > 1)
                                     @for($i= 1;$i <= $item[0]['qty'];$i++)
-                                        <div style="margin-top: 5px;padding: 5px;">
-                                             <span><strong>{{$item[0]['name']}}</strong></span>
-                                                <p><strong>HĐ #{{$billInfo['id']}}</strong></p>
-                                                <span><strong>{{number_format($item[0]['price'])}} VNĐ</strong></span>
+                                        <div >
+                                             <span style="font-size: 10px"><strong>#{{$billInfo['id']}}-{{$item[0]['name']}}: {{number_format($item[0]['price'])}} VNĐ</strong></span>
+                                    
                                         </div>
                                     @endfor
                                 @else
-                                    <div style="margin-top: 5px;padding: 5px;">
-                                         <span><strong>{{$item[0]['name']}}</strong></span>
-                                            <p><strong>HĐ #{{$billInfo['id']}}</strong></p>
-                                            <span><strong>{{number_format($item[0]['price'])}} VNĐ</strong></span>
+                                    <div >
+                                             <span style="font-size: 10px"><strong>#{{$billInfo['id']}}-{{$item[0]['name']}}: {{number_format($item[0]['price'])}} VNĐ</strong></span>
                                     </div>
                                 @endif
                             @endforeach
                         @endif
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
