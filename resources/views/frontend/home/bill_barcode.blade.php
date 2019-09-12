4bedf726a2d2f2aed577cc195141622610dbabd6
anhv <havanan95@gmail.com>
2019-09-12 16:28:41 +0700

    in 1

--- resources/views/frontend/home/bill_barcode.blade.php
+++ resources/views/frontend/home/bill_barcode.blade.php
@@ -3,20 +3,26 @@
 <script src="{{asset('js/payment.js')}}"></script>
 
 <div class="" id="billPrint">
-     @if(isset($cartInfo['list']) && count($cartInfo) > 0)
+     @if(isset($cartInfo['list']))
+        @if(count($cartInfo) > 1)
                             @foreach($cartInfo['list'] as $key => $item)
-                                @if($item[0]['qty'] > 1)
-                                    @for($i= 1;$i <= $item[0]['qty'];$i++)
-                                        <div >
-                                             <span style="font-size: 12px"><strong>#{{$billInfo['id']}}-{{$item[0]['name']}}: {{number_format($item[0]['price'])}} VNĐ</strong></span>
-                                    
+
+                                 @if($item[0]['qty'] > 1)
+                                        @for($i= 1;$i <= $item[0]['qty'];$i++)
+                                            <div style="height:23px">
+                                                 <span style="font-size: 7px"><strong>#{{$billInfo['id']}}-{{$item[0]['name']}}</strong></span>
+                                                 <br>
+                                                 <span style="font-size: 5px"><strong>{{number_format($item[0]['price'])}} VNĐ</strong></span>
+                                            </div>
+                                        @endfor
+                                    @else
+                                        <div style="height: 23px">
+                                                 <span style="font-size: 7px"><strong>#{{$billInfo['id']}}-{{$item[0]['name']}}</strong></span>
+                                                 <br>
+                                                 <span style="font-size: 5px"><strong>{{number_format($item[0]['price'])}} VNĐ</strong></span>
                                         </div>
-                                    @endfor
-                                @else
-                                    <div >
-                                             <span style="font-size: 12px"><strong>#{{$billInfo['id']}}-{{$item[0]['name']}}: {{number_format($item[0]['price'])}} VNĐ</strong></span>
-                                    </div>
-                                @endif
-                            @endforeach
-                        @endif
+                                    @endif   
+                            @endforeach                 
+         @endif                                                
+    @endif
 </div>
